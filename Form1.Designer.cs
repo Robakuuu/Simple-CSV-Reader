74a17dcf714386bd47603cb90a851dff74118044
Robakuuu <daniel.robaszkiewicz@wp.pl>
2020-07-30 16:42:50 +0200

    CSV Reader C#

---
+++ Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace CSVReader
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Wczytaj = new System.Windows.Forms.Button();
+            this.dataSet1 = new System.Data.DataSet();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Wczytaj
+            // 
+            this.Wczytaj.Location = new System.Drawing.Point(13, 13);
+            this.Wczytaj.Name = "Wczytaj";
+            this.Wczytaj.Size = new System.Drawing.Size(69, 30);
+            this.Wczytaj.TabIndex = 0;
+            this.Wczytaj.Text = "Load";
+            this.Wczytaj.UseVisualStyleBackColor = true;
+            this.Wczytaj.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataSet1
+            // 
+            this.dataSet1.DataSetName = "NewDataSet";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.DataSource = this.dataSet1;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 64);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(848, 428);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(764, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(96, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Choose CSV File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(224, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(416, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(88, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(68, 30);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Clear";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(719, 19);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(38, 20);
+            this.textBox2.TabIndex = 5;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(186, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Path:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(663, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Delimiter:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(872, 504);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Wczytaj);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "CSV Reader";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Wczytaj;
+        private System.Data.DataSet dataSet1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Globalization;
+
+namespace CSVReader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
+        List<List<string>> listA;
+        DataSet ds;
+        DataTable dt;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<List<string>> listA = new List<List<string>>();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable("CsvFile");
+
+
+            if (textBox1.Text != ""&&textBox2.Text!="")
+            {
+                string path = textBox1.Text;
+                string spliter = textBox2.Text;
+                char[] split = spliter.ToCharArray();
+                using (var reader = new StreamReader(path))
+                {
+                    
+                    int wiersze = 0;
+                    while (!reader.EndOfStream)
+                    {
+                        var line = reader.ReadLine();
+                        var values = line.Split(split[0]);
+                        listA.Add(new List<string>());
+                        for (int x = 0; x < values.Length; x++)
+                        {
+                            listA[wiersze].Add(values[x]);
+                        }
+
+                        if (wiersze == 0)
+                        {
+                            for (int x = 0; x < values.Length; x++)
+                            {
+                                dt.Columns.Add(new DataColumn(listA[wiersze][x], typeof(string)));
+                            }
+                        }
+                        else
+                        {
+                            DataRow dr = dt.NewRow();
+                            for (int x = 0; x < values.Length; x++)
+                            {
+                                dr[listA[0][x]] = listA[wiersze][x];
+
+                            }
+                            dt.Rows.Add(dr);
+
+                        }
+                        wiersze++;
+
+
+
+                    }
+                    ds.Tables.Add(dt);
+                    dataGridView1.AutoGenerateColumns = true;
+                    dataGridView1.DataSource = ds; // dataset
+                    dataGridView1.DataMember = "CsvFile";
+
+                    wiersze = 0;
+
+                    Console.WriteLine(ds);
+
+
+                }
+            }
+            else { textBox1.Text = ""; textBox2.Text = ""; MessageBox.Show("Set path/delimiter!"); }
+               
+
+           
+           
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+           
+            string FilePath = AppDomain.CurrentDomain.BaseDirectory;
+            
+            string argument = "/select, \"" + FilePath + "\"";
+            //Process.Start("explorer.exe", argument);
+
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+            openFileDialog1.InitialDirectory = FilePath;
+            this.openFileDialog1.Multiselect = false;
+            openFileDialog1.Filter = "csv files (*.csv)|*.csv|All files (*.*)|*.*";
+
+            openFileDialog1.ShowDialog();
+            textBox1.Text = openFileDialog1.FileName;
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<List<string>> listA = new List<List<string>>();
+            DataSet ds = new DataSet();
+            DataTable dt = new DataTable("CsvFile");
+            dataGridView1.AutoGenerateColumns = true;
+            dataGridView1.DataSource = ds; // dataset
+    
+            dataGridView1.Update();
+            dataGridView1.Refresh();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
